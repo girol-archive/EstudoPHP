f34d7056c532da7f888a75c6e0d10d931f199811
André Luiz Girol <andregirol@gmail.com>
2017-04-16 19:44:21 -0300

    Adicionando verificação de erros
    Adicionando diversas verificações de erros para:
    
    - Operação não selecionada
    - Divisão por zero

--- calculadora/dados.php
+++ calculadora/dados.php
@@ -3,17 +3,42 @@
 	$a = $_GET["a"];
 	$b = $_GET["b"];
 	$operacao =  $_GET["operacao"];
-	$result = [];
+	$result = null;
 
-	if($operacao == "Somar") {
+
+	if($operacao == "somar") {
+		
 		$result = $a + $b;
-	} else if($operacao == "Subtrair") {
+
+	} else if($operacao == "subtrair") {
+
 		$result = $a - $b;
-	} else if($operacao == "Multiplicar") {
+
+	} else if($operacao == "multiplicar") {
+
 		$result = $a * $b;
-	} else($operacao == "Dividir") {
+
+	// Aqui não precisa testar de novo, visto que só
+	} else if($operacao == "dividir") {
+
+		if($b == 0){
+			echo "Não é possível dividir por zero!<br>";
+			echo "<a href='index.html'>Voltar</a>";
+			exit();//encerra o script imediatamente
+		}
+
 		$result = $a / $b;
+
+	// Caso o usuário não escolha nada, mostra mensagem de erro
+	} else {
+		
+		echo "Escolha uma das operações";
+		echo "<br>";
+		echo "<a href='index.html'>Voltar</a>";
+		exit();//encerra o script imediatamente
 	}
 
-	echo "O resultado é: ".$result;
-?>
+	echo "O resultado é: ". $result;
+	echo "<br>";
+	echo "<a href='index.html'>Calcular novamente</a>";
+
