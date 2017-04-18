9638ab870676b59d95dc884750eb6fe4a5d5e066
Renata Kilian <renatakiliandev@gmail.com>
2017-04-18 15:26:45 -0300

    Update dados.php
    Calculadora básica com function

--- calculadora/dados.php
+++ calculadora/dados.php
@@ -1,19 +1,56 @@
 <?php
 
-	$a = $_GET["a"];
+$a = $_GET["a"];
 	$b = $_GET["b"];
 	$operacao =  $_GET["operacao"];
-	$result = [];
+	$result = null;
 
-	if($operacao == "Somar") {
-		$result = $a + $b;
-	} else if($operacao == "Subtrair") {
-		$result = $a - $b;
-	} else if($operacao == "Multiplicar") {
-		$result = $a * $b;
-	} else($operacao == "Dividir") {
-		$result = $a / $b;
+	function somar($a, $b) {
+		return $a + $b;
 	}
 
-	echo "O resultado é: ".$result;
-?>
+	function subtrair($a, $b) {
+		return $a - $b;
+	}
+
+	function multiplicar($a, $b) {
+		return $a * $b;
+	}
+	function dividir($a, $b) {
+		return $a / $b;
+	}
+
+	switch ($operacao) {
+		case 'somar': 
+			somar($a, $b);
+			break;
+
+		case 'subtrair':
+			subtrair($a, $b);			
+			break;
+
+		case 'multiplicar':
+			multiplicar($a, $b);		
+			break;
+
+		case 'dividir':
+			if($b == 0) {
+			echo "Não é possível dividir por zero!";
+			echo "<br/>";
+			echo "<a href='index.html'>Voltar</a>";
+			exit();
+		}
+			dividir($a, $b);			
+			break;
+		
+		default:
+			echo "Escolha uma das operações!";
+			echo "<br/>";
+			echo "<a href='index.html'>Voltar</a>";
+	}
+
+	//if($result !== null) { //com o if não aparece retorno
+		echo "O resultado é: ".$result;
+		echo "<br/>";
+		echo "<a href='index.html'>Voltar</a>";
+	//}
