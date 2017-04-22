8c67d323c68ca460b0f2a9056cbec365f09b499f
André Luiz Girol <andregirol@gmail.com>
2017-04-22 14:59:03 -0300

    Correção dos commits anteriores
    Erros:
    - Classe instanciada em local errado
    - Calculador de operações estava fora da classe
    - Diversas correções de atribuição de variáveis

--- calculadora_oo/calculadora.php
+++ calculadora_oo/calculadora.php
@@ -1,60 +1,96 @@
 <?php
+/*
+Uma classe tem o objetivo de abstrair o código das operações
+que você executa no código, então, normalmente não se atribuem valores
+diretamente na classe, a menos que necessário.
+
+No caso, a variável $result precisa de um dado atribuído por nós e inicializado
+para verificação posterior.
+
+*/
+
+
 class Calculadora {
 
-	public $a = $_GET["a"];
-	public $b = $_GET["b"];
-	public $operacao =  $_GET["operacao"];
+	public $a;
+	public $b;
+	public $operacao;
 	public $result = null;
 
-	public function somar($a, $b) {
-		return $a + $b;
-	}
-	public function subtrair($a, $b) {
-		return $a - $b;
-	}
-	public function multiplicar($a, $b) {
-		return $a * $b;
-	}
-	public function dividir($a, $b) {
-		return $a / $b;
-	}
-}
+	/*
+	 Método construtor para passar à nossa classe
+	 todas as informações e ela se encarregará de realizar
+	 os cálculos
 
-	switch ($operacao) {
-		case 'somar': 
-			$result = new Calculadora();
-			$result->somar($a, $b);
-			break;
+	*/
 
-		case 'subtrair':
-			$result = new Calculadora();
-			$result->subtrair($a, $b);		
-			break;
+	public function __construct($num1, $num2){
+		$this->a = $num1;
+		$this->b = $num2;
 
-		case 'multiplicar':
-			$result = new Calculadora();
-			$result->multiplicar($a, $b);	
-			break;
+	}
 
-		case 'dividir':
-			if($b == 0) {
-				echo "Não é possível dividir por zero!";
+	public function somar() {
+		return $this->a + $this->b;
+	}
+	public function subtrair() {
+		return $this->a - $this->b;
+	}
+	public function multiplicar() {
+		return $this->a * $this->b;
+	}
+	public function dividir() {
+		return $this->a / $this->b;
+	}
+
+	/*
+	Toda a tarefa de calcular deve ser dada à calculadora,
+	portanto, criamos um método calcular
+	*/ 
+	public function calcular($operacao) {
+
+		switch ($operacao) {
+			case 'somar': 
+				// Normalmente não se instancia uma classe dentro dela mesma!
+				
+				/*
+				$this, para acessar o próprio atributo.
+				O mesmo vale para os métodos
+				*/
+				$this->result = $this->somar();
+				break;
+
+			case 'subtrair':
+				
+				$this->result = $this->subtrair();		
+				break;
+
+			case 'multiplicar':
+				
+				$this->result = $this->multiplicar();	
+				break;
+
+			case 'dividir':
+				if($this->b == 0) {
+					echo "Não é possível dividir por zero!";
+					echo "<br/>";
+					echo "<a href='index.html'>Voltar</a>";
+					exit();
+				}
+				
+				$this->result = $this->dividir();		
+				break;
+			
+			default:
+				echo "Escolha uma das operações!";
 				echo "<br/>";
 				echo "<a href='index.html'>Voltar</a>";
-				exit();
-			}
-			$result = new Calculadora();
-			$result->dividir($a, $b);		
-			break;
-		
-		default:
-			echo "Escolha uma das operações!";
+		}
+
+		if($this->result !== null) {
+			echo "O resultado é: ".$this->result;
 			echo "<br/>";
 			echo "<a href='index.html'>Voltar</a>";
+		}
 	}
-
-	if($result !== null) {
-		echo "O resultado é: ".$result;
-		echo "<br/>";
-		echo "<a href='index.html'>Voltar</a>";
-	}
+}
--- calculadora_oo/dados.php
+++ calculadora_oo/dados.php
@@ -4,4 +4,12 @@
 	$a = $_GET["a"];
 	$b = $_GET["b"];
 	$operacao =  $_GET["operacao"];
-	$result = null;
+
+	/*
+	Instancia a nossa classe, carregando tudo
+	o que programamos em calculadora.php
+	na variável $calc
+	*/ 
+	$calc = new Calculadora($a,$b);
+
+	$calc->calcular($operacao);
