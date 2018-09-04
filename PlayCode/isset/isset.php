2a49838be65b3caa93905731ed45bb32fa6f61f7
André Luiz Girol <andregirol@gmail.com>
2018-09-04 11:35:34 -0300

    Atualizando exemplo de isset()

--- PlayCode/isset/isset.php
+++ PlayCode/isset/isset.php
@@ -7,6 +7,19 @@ if(empty($_POST['teste'])){
 	var_dump($_POST['teste']);
 }
 
+if (isset($_POST['teste'])){
+    echo "A variável está setada...";
+	var_dump($_POST['teste']);
+}
+
 echo "<br/><br/>";
 echo "<a href='index.html'>Voltar</a>";
+echo "<hr>";
+echo "Testando empty sem existir";
+
+if(empty($olar)){
+    echo "tá vazia e não chamada em lugar algum";
+    var_dump($olar);
+}
+
 exit;
