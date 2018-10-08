a1f370d2df0ea28640a1484d7d848fd6b9586c1a
André Luiz Girol <andregirol@gmail.com>
2018-10-08 17:37:18 -0300

    if/else Shorthand operators (?:)

---
+++ PlayCode/shorthand_operator.php
@@ -0,0 +1,18 @@
+<?php
+
+# Using classic if/else
+
+$condition = false;
+
+if($condition){
+    echo "is true...";
+} else {
+    echo "is false...";
+}
+
+echo "<hr>";
+
+$result = $condition ? "troo..." : "phalsey...";
+
+echo $result;
+
